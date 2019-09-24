b5bbaa36dc40b996cc92328114738c37013a81e2
zhumengyua <892244044@qq.com>
2019-09-24 20:25:57 +0800

    更新gutter

--- src/col.vue
+++ src/col.vue
@@ -1,6 +1,11 @@
 <template>
-  <div class="col" :class="[`col-${span}`]">
-    <slot></slot>
+  <div
+    class="col"
+    :class="[span && `col-${span}`,offset && `offset-${offset}`]"
+    :style="{paddingLeft:gutter/2+'px',paddingRight:gutter/2+'px'}"
+  >
+  <div style="border:1px solid green;height:100px">
+    <slot></slot></div>
   </div>
 </template>
 
@@ -8,7 +13,15 @@
 export default {
   name: "GuluCol",
   props: {
-    span:[Number, String]
+    span: [Number, String],
+    offset: [Number, String],
+    gutter:[Number,String]
+  },
+  created(){
+    console.log('col created')
+  },
+  mounted(){
+    console.log('col mounted')
   }
 };
 </script>
@@ -17,13 +30,16 @@ export default {
 .col {
   width: 50%;
   height: 100px;
-  background-color: #999;
-  border: 1px solid red;
-
   $class-prefix: col-;
   @for $n from 1 through 24 {
     &.#{$class-prefix}#{$n} {
       width: ($n/24) * 100%;
+    }
+    $class-prefix: offset-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        margin-left: ($n/24) * 100%;
+      }
     }
   }
 }
--- src/row.vue
+++ src/row.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="row">
+    <div class="row" :style="{marginLeft:-gutter/2+'px',marginRight:-gutter/2+'px'}">
 <slot>
     </div>
 </template>
@@ -7,14 +7,32 @@
 <script>
     export default {
         name:'GuluRow',
-        props:{
-span:Number
-        }
+ props:{
+     gutter:{
+         type:[Number,String]
+     }
+ },
+ created(){
+console.log('row created')
+ },
+ mounted(){
+     console.log('row mounted')
+     console.log(this.$children)
+     this.$children.forEach((vm)=>{
+         vm.gutter=this.gutter
+     })
+ }
     }
+  var div=document.createElement('div')//created
+  var childDiv =document.createElement('div')//child cerated
+  div.appendChild(childDiv)//child mounted
+  document.body.appendChild(div)
 </script>
 
 <style lang="scss" scoped>
+
 .row{
-    display:flex
+    display:flex;
+    margin:0 -10px;
 }
 </style>
